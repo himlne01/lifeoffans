93596d6acdbb83335edf044e86992622fef05672
himlne01 <33374005+himlne01@users.noreply.github.com>
2018-04-17 20:36:05 -0500

    more

--- router.py
+++ router.py
@@ -2,10 +2,14 @@ from flask import Flask, url_for, redirect, render_template, request, Response, 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/index")
 def index():
     return render_template('index.html')
 
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/contact")
+def about():
+    return render_template('contact.html')
