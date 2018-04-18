9c88dc1024afcbae15755650891ca6798a05608a
himlne01 <33374005+himlne01@users.noreply.github.com>
2018-04-17 21:40:32 -0500

    things

--- router.py
+++ router.py
@@ -13,3 +13,23 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
+
+@app.route("/a-collection-of-two-poems-by-emma-and-nell")
+def collection():
+    return render_template('a-collection-of-two-poems-by-emma-and-nell.html')
+
+@app.route("/a-percy-jackson-musical")
+def musical():
+    return render_template('a-percy-jackson-musical.html')
+
+@app.route("/more-than-a-word-on-culture")
+def culture():
+    return render_template('a-percy-jackson-musical.html')
+
+@app.route("/what-is-bts-doing-now")
+def bts():
+    return render_template('what-is-bts-doing-now.html')
+
+@app.route("/what-is-kpop")
+def kpop():
+    return render_template('what-is-kpop.html')
