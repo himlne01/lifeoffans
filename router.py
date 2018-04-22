f5d93307bbf2c0bde9f35cf3b7cdd3bd16a21eec
himlne01 <33374005+himlne01@users.noreply.github.com>
2018-04-22 16:56:03 -0500

    check?

--- router.py
+++ router.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, redirect, render_template, request, Response, 
 
 app = Flask(__name__)
 
-@app.route("/index")
+@app.route("/")
 def index():
     return render_template('index.html')
 
