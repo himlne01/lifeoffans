eb09f01464cf335d74aa79dcd9e2cd858aa23eb5
himlne01 <33374005+himlne01@users.noreply.github.com>
2018-05-08 01:01:13 -0500

    last blg

--- router.py
+++ router.py
@@ -14,22 +14,22 @@ def about():
 def contact():
     return render_template('contact.html')
 
-@app.route("/a-collection-of-two-poems-by-emma-and-nell")
+@app.route("/blog/a-collection-of-two-poems-by-emma-and-nell")
 def collection():
-    return render_template('blog/a-collection-of-two-poems-by-emma-and-nell.html')
+    return render_template('/a-collection-of-two-poems-by-emma-and-nell.html')
 
-@app.route("/a-percy-jackson-musical")
+@app.route("/blog/a-percy-jackson-musical")
 def musical():
-    return render_template('blog/a-percy-jackson-musical.html')
+    return render_template('/a-percy-jackson-musical.html')
 
-@app.route("/more-than-a-word-on-culture")
+@app.route("/blog/more-than-a-word-on-culture")
 def culture():
-    return render_template('/blog/more-than-a-word-on-culture.html')
+    return render_template('/more-than-a-word-on-culture.html')
 
-@app.route("/what-is-bts-doing-now")
+@app.route("/blog/what-is-bts-doing-now")
 def bts():
-    return render_template('blog/what-is-bts-doing-now.html')
+    return render_template('/what-is-bts-doing-now.html')
 
-@app.route("/what-is-kpop")
+@app.route("/blog/what-is-kpop")
 def kpop():
-    return render_template('blog/what-is-kpop.html')
+    return render_template('/what-is-kpop.html')
