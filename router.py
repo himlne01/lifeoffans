ac202f4c2994b5bc4ff255a9dfafe86858a7f198
himlne01 <33374005+himlne01@users.noreply.github.com>
2018-05-08 01:23:18 -0500

    blog doesn't go under render

--- router.py
+++ router.py
@@ -16,20 +16,20 @@ def contact():
 
 @app.route("/a-collection-of-two-poems-by-emma-and-nell")
 def collection():
-    return render_template('blog/a-collection-of-two-poems-by-emma-and-nell.html')
+    return render_template('/a-collection-of-two-poems-by-emma-and-nell.html')
 
 @app.route("/a-percy-jackson-musical")
 def musical():
-    return render_template('blog/a-percy-jackson-musical.html')
+    return render_template('/a-percy-jackson-musical.html')
 
 @app.route("/more-than-a-word-on-culture")
 def culture():
-    return render_template('blog/more-than-a-word-on-culture.html')
+    return render_template('/more-than-a-word-on-culture.html')
 
 @app.route("/what-is-bts-doing-now")
 def bts():
-    return render_template('blog/what-is-bts-doing-now.html')
+    return render_template('/what-is-bts-doing-now.html')
 
 @app.route("/what-is-kpop")
 def kpop():
-    return render_template('blog/what-is-kpop.html')
+    return render_template('/what-is-kpop.html')
